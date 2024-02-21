bdc5085a6949b2fcc80ee320c0f27b7ba83cdcbf
Letisha Lorina <letishal@un-j6.kzn.21-school.ru>
2024-02-21 21:07:45 +0300

    ex02

---
+++ src/ex02/Tests/src/test/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO product
+VALUES (1, 'Product A', 10.99);
+INSERT INTO product
+VALUES (2, 'Product B', 15.49);
+INSERT INTO product
+VALUES (3, 'Product C', 20.99);
+INSERT INTO product
+VALUES (4, 'Product D', 7.99);
+INSERT INTO product
+VALUES (5, 'Product E', 12.99);
